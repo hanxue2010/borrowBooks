2aac24c6aa754d948db0fea0a7a3fddfba7136c3
hanxue2010 <1181424693@qq.com>
2019-04-15 00:40:26 +0800

    添加包和测试文件后的提交

---
+++ booksManager/src/com/chuxing/borrowBooks/booksManager/test.java
@@ -0,0 +1,7 @@
+package com.chuxing.borrowBooks.booksManager;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("this is a test in the booksManager");
+    }
+}
